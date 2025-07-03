4051afb9d9ff47909506cdcbe3d6edb5eb371e5d
Carlos Silva <cmiguelp@gmail.com>
2025-07-03 02:29:32 +0100

    added acquire_input_focus to shared.lua

--- modules/shared.lua
+++ modules/shared.lua
@@ -1,4 +1,9 @@
 
+function acquire_input_focus()
+	msg.post(".", "acquire_input_focus")
+end
+
 function triger_level_load(level)
 	msg.post('game:/level_selector#loader', "load", { level = level })
 end
+
